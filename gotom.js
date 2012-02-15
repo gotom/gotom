d2115b31d13937f1e6eb598f8e778ab1ba7b43e3
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2012-02-15 10:50:24 +0100

    asdf

--- gotom.js
+++ gotom.js
@@ -1,13 +1,3 @@
 function main(args){
-<<<<<<< HEAD
-	var test1;
-	test=0;
-	test2fsdfasdfasdf
-	ffjhhadoif
-	asdfasdf
-	test("test");
-	bllaaasdfasdfasdfasdfdupadupa
-=======
-changes next
->>>>>>> 163b39f867582d1a444ff15c8268d6fcc6af34ae
+test
 }
