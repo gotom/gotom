aabc042b0a8883a6a3ccd9ff6334070f1954aa71
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2013-01-24 18:27:30 +0100

    asdfasdf

--- gosia.js
+++ gosia.js
@@ -1,4 +1,14 @@
 asdfasdf
+messages['test']
+asdfasdf
 
+<<<<<<< Upstream, based on ea46cb3ceebeeb74810ef297c8aafbaf58312fd8
 asdfasdf
 d
+=======
+messages['test"test']
+
+messages["test\"test"]
+
+'test\'test'
+>>>>>>> 5d4ca40 sdfasdf
